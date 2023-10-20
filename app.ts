4402a1a2cfd651b725076c6b957d406e83c66351
Kirill Hrudzinski <604karev@gmail.com>
2023-10-20 11:02:45 +0200

    add type guards

--- app.ts
+++ app.ts
@@ -72,7 +72,7 @@ interface Admin {
   role: number;
 }
 
-function typeMapping(user: User): Admin {
+function typeMaping(user: User): Admin {
   return {
     name: user.name,
     role: 1,
@@ -83,10 +83,36 @@ function isString(x: string | number): x is string {
   return typeof x === "string";
 }
 
+function isAdmin(user: User | Admin): user is Admin {
+  return (user as Admin).role !== undefined;
+}
+
 function logIn(id: string | number) {
   if (isString(id)) {
     console.log(id);
   } else {
     console.log(id);
+  }
+}
+
+function setRole(user: User | Admin) {
+  if (isAdmin(user)) {
+    console.log(user.role);
+  } else {
+    console.log(user.email);
+  }
+}
+
+type Responce = IResponseFailed | IResponseSuccess;
+
+function isSucess(res: Responce): res is IResponseSuccess {
+  return (res as IResponseSuccess).status === PaymentStatus.SUCCESS;
+}
+
+function getIdFromData(res: Responce): number {
+  if (isSucess(res)) {
+    return res.data.databaseId;
+  } else {
+    return res.data.errorCode;
   }
 }
