9e4f8136d33f6dfc5718e41399602893ec3a7b9c
Kirill Hrudzinski <604karev@gmail.com>
2023-12-20 10:54:47 +0100

    add behavioral patterns

--- src/behavioralPattern.ts
+++ src/behavioralPattern.ts
@@ -213,7 +213,7 @@ class DocumentItem {
   }
 
   deleteDoc() {
-    this.state.deleve();
+    this.state.delete();
   }
 }
 
@@ -225,7 +225,7 @@ abstract class DocumentItemState {
     this.item = this.item;
   }
   public abstract publish(): void;
-  public abstract deleve(): void;
+  public abstract delete(): void;
 }
 
 class DraftDocumentItmeState extends DocumentItemState {
@@ -237,7 +237,7 @@ class DraftDocumentItmeState extends DocumentItemState {
     console.log(`Text was sent`);
     this.item.setState(new PublishDocumentItmeState());
   }
-  public deleve(): void {
+  public delete(): void {
     console.log("Document deleted");
   }
 }
@@ -250,7 +250,7 @@ class PublishDocumentItmeState extends DocumentItemState {
   public publish(): void {
     console.log("You can't publish publisheddocument");
   }
-  public deleve(): void {
+  public delete(): void {
     throw new Error("remove from published");
     this.item.setState(new DraftDocumentItmeState());
   }
@@ -263,3 +263,239 @@ item.publishDoc();
 console.log(item.getState());
 item.deleteDoc();
 console.log(item.getState());
+
+//Strategy
+
+class AuthUser {
+  githubToken: string;
+  jwtToken: string;
+}
+
+interface IAuthStrategy {
+  auth(user: AuthUser): boolean;
+}
+
+class Auth {
+  constructor(private strategy: IAuthStrategy) {
+    // this.setStrategy(strategy);
+  }
+
+  setStrategy(strategy: IAuthStrategy) {
+    this.strategy = strategy;
+  }
+
+  public authUser(user: AuthUser): boolean {
+    return this.strategy.auth(user);
+  }
+}
+
+class JWTStrategy implements IAuthStrategy {
+  auth(user: AuthUser): boolean {
+    if (user.jwtToken) {
+      return true;
+    }
+    return false;
+  }
+}
+
+class GithubStrategy implements IAuthStrategy {
+  auth(user: AuthUser): boolean {
+    if (user.githubToken) {
+      return true;
+    }
+    return false;
+  }
+}
+
+const authUser = new AuthUser();
+authUser.jwtToken = "toket";
+const authStrategy = new Auth(new JWTStrategy());
+console.log(authStrategy.authUser(authUser));
+authStrategy.setStrategy(new GithubStrategy());
+console.log(authStrategy.authUser(authUser));
+
+//Iterator
+
+interface IIterator<T> {
+  current(): T | undefined;
+  next(): T | undefined;
+  prev(): T | undefined;
+  index(): number;
+}
+
+class PriorityIterator implements IIterator<Task> {
+  private position: number = 0;
+  private taskList: TaskList;
+  constructor(taskList: TaskList) {
+    taskList.sortByPriority();
+    this.taskList = taskList;
+  }
+  current(): Task | undefined {
+    return this.taskList.getTasks()[this.position];
+  }
+  next(): Task | undefined {
+    this.position += 1;
+    return this.taskList.getTasks()[this.position];
+  }
+  prev(): Task | undefined {
+    this.position -= 1;
+    return this.taskList.getTasks()[this.position];
+  }
+
+  index(): number {
+    return this.position;
+  }
+}
+
+class Task {
+  constructor(public priority: number) {}
+}
+
+class TaskList {
+  private tasks: Task[] = [];
+
+  public sortByPriority() {
+    this.tasks = this.tasks.sort((a, b) => {
+      if (a.priority > b.priority) {
+        return 1;
+      } else if (a.priority == b.priority) {
+        return 0;
+      } else {
+        return -1;
+      }
+    });
+  }
+
+  public addTask(task: Task) {
+    this.tasks.push(task);
+  }
+  public getTasks() {
+    return this.tasks;
+  }
+  public countTasks() {
+    return this.tasks.length;
+  }
+  public getIterator() {
+    return new PriorityIterator(this);
+  }
+}
+
+const taskList = new TaskList();
+taskList.addTask(new Task(8));
+taskList.addTask(new Task(1));
+taskList.addTask(new Task(3));
+const iterator = taskList.getIterator();
+console.log(iterator.current());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.prev());
+console.log(iterator.index());
+
+//Template Method
+
+class Form {
+  constructor(public name: string) {}
+}
+
+abstract class SaveForm<T> {
+  public save(form: Form) {
+    const res = this.fill(form);
+    this.log(res);
+    this.send(res);
+  }
+
+  protected abstract fill(form: Form): T;
+  protected log(data: T): void {
+    console.log(data);
+  }
+
+  protected abstract send(data: T): void;
+}
+
+class FirstAPI extends SaveForm<string> {
+  protected fill(form: Form): string {
+    return form.name;
+  }
+  protected send(data: string): void {
+    console.log(`Send ${data}`);
+  }
+}
+
+class SecondAPI extends SaveForm<{ fullName: string }> {
+  protected fill(form: Form): { fullName: string } {
+    return { fullName: form.name };
+  }
+  protected send(data: { fullName: string }): void {
+    console.log(`Send ${data}`);
+  }
+}
+
+const form1 = new FirstAPI();
+form1.save(new Form("Dik"));
+
+const form2 = new SecondAPI();
+form2.save(new Form("Dik"));
+
+//Observer
+
+interface IObserver {
+  update(subject: ISubject): void;
+}
+interface ISubject {
+  attach(observer: IObserver): void;
+  detach(observer: IObserver): void;
+  notify(): void;
+}
+
+class Lead {
+  constructor(public name: string, public phone: string) {}
+}
+
+class NewLead implements ISubject {
+  private observers: IObserver[] = [];
+  public state: Lead;
+
+  attach(observer: IObserver): void {
+    if (this.observers.includes(observer)) {
+      return;
+    } else {
+      this.observers.push(observer);
+    }
+  }
+  detach(observer: IObserver): void {
+    const oberverIndex = this.observers.indexOf(observer);
+    if (oberverIndex === -1) {
+      return;
+    }
+    this.observers.slice(oberverIndex, 1);
+  }
+  notify(): void {
+    for (const observer of this.observers) {
+      observer.update(this);
+    }
+  }
+}
+
+class NotificationService implements IObserver {
+  update(subject: ISubject): void {
+    console.log("NotificationService got notiffication");
+    console.log(subject);
+  }
+}
+
+class LeadService implements IObserver {
+  update(subject: ISubject): void {
+    console.log("LeadService got notiffication");
+    console.log(subject);
+  }
+}
+
+const subject = new NewLead();
+subject.state = new Lead("Jack", "000");
+const s1 = new NotificationService();
+const s2 = new LeadService();
+subject.attach(s1);
+subject.attach(s2);
+subject.notify();
+subject.detach(s1)
+subject.notify();
